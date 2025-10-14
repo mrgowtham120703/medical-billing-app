82099de205d918e530ace3a753de9dfe88f3aa85
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-14 11:10:54 -0700

    configured db.js

--- backend/server.js
+++ backend/server.js
@@ -3,3 +3,9 @@ import dotenv from  'dotenv';
 import morgan from 'morgan';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(morgan('dev'));
---
+++ backend/src/config/db.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+module.exports = async function connectDB() {
+    const uri = process.env.MONGO_URI || 'mongodb://localhost:27017/medbilling';
+    try {
+        await mongoose.connect(uri);
+        console.log('MongoDB Connected');
+    } catch (error) {
+        console.log('MongoDB connection error', error)
+        process.exit(1);
+    }
+}
