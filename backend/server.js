4b932e2fad0478800f61d0b15d7c805a9c552eb2
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-17 21:39:28 -0700

    Routes added in server.js

--- backend/server.js
+++ backend/server.js
@@ -13,3 +13,11 @@ app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 connectDB();
+
+// routes
+app.use('/api/providers', require('./src/routes/providers'));
+app.use('/api/patients', require('./src/routes/patients'));
+app.use('/api/claims', require('./src/routes/claims'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server Listening on ${PORT}`));
