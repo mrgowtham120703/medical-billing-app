c09c23859b87ebf8093ab4e676d360b2fef86dc4
mrgowtham120703 <gowthammr120703@gmail.com>
2025-10-18 22:31:12 -0700

    commit for frontend

--- backend/src/controllers/claimContoller.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const Patient = require('../models/Patient');
-
-router.post('/', async (req, res)=>{
-    const patient = new Patient(req.body);
-})
--- backend/src/models/Provider.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const ProviderSchema = new mongoose.Schema({
-    name: String,
-    npi: String,
-    address: Object,
-    contact: Object,
-}, { timestamps: true });
-module.exports = mongoose.model('Provider', ProviderSchema);
